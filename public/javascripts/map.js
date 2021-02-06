c399dac1d9f6838c2d76009ffe6f67c8b3aa649b
Kevin Bagnato <kevinbagnato@gmail.com>
2021-02-06 18:05:34 -0500

    file cleanup

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -2,7 +2,6 @@ $('document').ready(function() {
     initMapbox(); 
 });
 
-// 
 function initMapbox() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYmFnYWRvbnV0cyIsImEiOiJja2tzbnVyMDMwbnIyMnhxbWVxdnRoc3Z1In0.IXWNtJpEBnXTXDyKVVRC5w';
     var map = new mapboxgl.Map({
