e3d2bb33590e9ecb9495839d4bd15ac0fc18aeef
Kevin Bagnato <kevinbagnato@gmail.com>
2021-02-07 16:24:53 -0500

    create astro page

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,11 @@ router.get('/smokiesApril21', function(req, res, next) {
   res.render('smokiesApril21', { title: TITLE });
 });
 
+/* GET astrophotography page. */
+router.get('/astro', function(req, res, next) {
+  res.render('astro', { title: TITLE });
+});
+
 /* GET About website page. */
 router.get('/aboutWebsite', function(req, res, next) {
   res.render('aboutWebsite', { title: TITLE });
