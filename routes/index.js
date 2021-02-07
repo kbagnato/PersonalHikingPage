f8631fd0f6a45179ba2c90a97bf45eeb2bab2c77
Kevin Bagnato <kevinbagnato@gmail.com>
2021-02-06 21:19:30 -0500

    update document title

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const TITLE = "Bagadonuts Hiking"
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: TITLE});
 });
 
 /* GET Smokies Apeil 21 page. */
 router.get('/smokiesApril21', function(req, res, next) {
-  res.render('smokiesApril21', { title: 'Express' });
+  res.render('smokiesApril21', { title: TITLE });
 });
 
 /* GET About website page. */
 router.get('/aboutWebsite', function(req, res, next) {
-  res.render('aboutWebsite', { title: 'Express' });
+  res.render('aboutWebsite', { title: TITLE });
 });
 
 module.exports = router;
