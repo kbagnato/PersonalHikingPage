a4006537966f809c72e5e0332f54b7dc7ffa7f16
Kevin Bagnato <kbagnato@ramapo.edu>
2023-09-05 13:42:22 -0700

    add slideshow to homepage

---
+++ public/javascripts/slideshow.js
@@ -0,0 +1,35 @@
+// code from w3schools.com
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  console.log('showing slide ' + n)
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  if (slides[slideIndex-1] != undefined)
+    slides[slideIndex-1].style.display = "block";
+  if (dots[slideIndex-1] != undefined)
+    dots[slideIndex-1].className += " active";
+}
