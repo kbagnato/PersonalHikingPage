da53e87d4b7d0b2e60883da25c0c503c10a6a5e3
Kevin Bagnato <kevinbagnato@gmail.com>
2023-09-14 21:09:49 -0600

    add individual gear links

--- public/javascripts/bagadonuts.js
+++ public/javascripts/bagadonuts.js
@@ -24,17 +24,18 @@ function populateList(list) {
   let gearCard;
 
   for (idx in list) {
-    gearCard = `<div class='card gear-card'>`;
+    gearCard = `<div class='card gear-card' id='item-list[idx].id' >`;
     if (list[idx].pictures)
       gearCard += `<img class='card-img-top' src='/images/gear/${list[idx].pictures[0]}.jpg' alt="Image coming soon">`;
     else
       gearCard += '<p>No image found.</p>' 
     gearCard += `
+      <a href='/gear/${list[idx].id}'><span class='gear-card-link'></span></a>
       <div class='card-body'>
           <h5 class='card-title'>${list[idx].name}</h5
           <h6 class='card-subtitle.text-muted'>${list[idx].desc}</h6>
         </div>
-      </div>`;
+    </div>`; // end card
     
     $('.bagadonuts-cards').append(gearCard);
   }
